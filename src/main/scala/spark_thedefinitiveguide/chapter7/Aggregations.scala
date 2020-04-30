8160bff6933adae67b7634d9064672e3eb62a67d
hkoller <hannes.koller@ait.ac.at>
2020-04-30 11:59:03 +0200

    add more aggregation examples

--- src/main/scala/spark_thedefinitiveguide/chapter7/Aggregations.scala
+++ src/main/scala/spark_thedefinitiveguide/chapter7/Aggregations.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.{RelationalGroupedDataset, SparkSession}
 import org.apache.spark.sql.functions.{col, expr, lit, udf}
 import org.apache.spark.sql.functions.{approx_count_distinct, avg, count, countDistinct, first, kurtosis, last, max, min, skewness, sum, var_samp}
-import org.apache.spark.sql.functions.{collect_list, collect_set, to_date, rank, dense_rank, row_number}
+import org.apache.spark.sql.functions.{collect_list, collect_set, to_date, rank, dense_rank, row_number, grouping_id}
 
 object Aggregations {
 
@@ -72,6 +72,9 @@ object Aggregations {
     val window = Window.partitionBy('CustomerId, 'date).orderBy('Quantity.desc).rowsBetween(Window.unboundedPreceding, Window.currentRow)
     df2.select('CustomerId, 'date, sum('Quantity).over(window), max('Quantity).over(window)).show()
 
+    // contrary to groupBy we can also select columns which are not in the Grouping defined by Window.partitionBy()
+    df2.select('CustomerId, 'date, 'Quantity, sum('Quantity).over(window), max('Quantity).over(window)).show()
+
     // (it's possible to use multiple different Window definitions in the same select)
     val window2 = Window.partitionBy('date).orderBy('Quantity.desc).rangeBetween(-3, 0)
     df2.select('CustomerId, 'date, sum('Quantity).over(window), sum('Quantity).over(window2)).show(false)
@@ -91,6 +94,94 @@ object Aggregations {
     //  - rank: when there are ties the next ranks are skipped:  1 2 3 3 5 6
     //  - dense_rank: leaves NO GAPS when there are ties: 1 2 3 3 4 5
     df2.select('CustomerId, 'Quantity, dense_rank().over(window3)).show(100)
+
+
+    //
+    // Grouping Sets  (only in SQL)
+    //
+
+    // first, need to eliminate null values from the grouping columns
+    df.na.drop("any", Seq("CustomerId", "StockCode")).createOrReplaceTempView("dfTable2")
+
+    spark.sql("SELECT CustomerId, stockCode, sum(Quantity) FROM dfTable2" +
+      " GROUP BY customerId, StockCode" +
+      " GROUPING SETS((customerId, stockCode), (stockCode))" +
+      " ORDER BY CustomerId DESC, stockCode DESC").show(100000)
+
+    // this will produce the "normal" grouped output you would expect, but also add an aggregate row for every stockCode (where customerId=null)
+    // for details see https://www.waitingforcode.com/apache-spark-sql/grouping-sets-apache-spark-sql/read
+
+
+    //
+    // Rollups
+    //
+
+    // a Rollup "computes hierarchical subtotals from left to right” : https://mungingdata.com/apache-spark/aggregations/
+
+    // first, need to eliminate nulls from the rollup columns
+    val dfNotNull = df2.na.drop("any")
+
+    val rollup:RelationalGroupedDataset = dfNotNull.rollup('date, 'Country)
+    // this basically defines a nested loop:
+    // for each date
+    //      for each country
+    //          create_row:  (date, country, aggregate( values(date, country) )
+    //      create_row: (date, country=null, aggregate(values(date))
+    // create_row: (date=null, country=null, aggregate(all_values))
+
+    // now define the aggregates we want to execute over the rollup:
+    val rollup_result = rollup.agg(sum('Quantity).as("sum")).select('date, 'Country, 'sum)
+
+    rollup_result.where("date IS NOT NULL AND Country IS NOT NULL").show()  // one row for each country/date combination
+    rollup_result.where("date IS NOT NULL AND Country IS NULL").show()  // one row per date
+    rollup_result.where("date IS NULL AND Country IS NULL").show()  // grand total
+
+    //
+    // Cubes
+    //
+
+    // a Cube is similar to a Rollup but it
+    // “applies the aggregate expressions to ALL POSSIBLE COMBINATIONS of the grouping columns”
+
+    val cube:RelationalGroupedDataset = dfNotNull.cube('date, 'Country)
+    val cube_result = cube.agg(sum('Quantity).as("sum")).select('date, 'Country, 'sum)
+
+    cube_result.where("date IS NOT NULL AND Country IS NOT NULL").show()  // one row for each country/date combination
+    cube_result.where("date IS NULL AND Country IS NOT NULL").show()  // one row for each country
+    cube_result.where("date IS NOT NULL AND Country IS NULL").show()  // one row for each date
+    cube_result.where("date IS NULL AND Country IS NULL").show()  // grand total
+
+
+    //
+    // Grouping Metadata
+    //
+
+    // grouping_id() adds a column that indicates the level of aggregation for each row (higher number = higher aggregation level)
+
+    rollup.agg(grouping_id(), sum('Quantity)).show()
+    // level 0 -> date!=null && country!=null  --> lowest level of aggregation (a row for each date/country combination)
+    // level 1 -> date!=null && country==null
+    // level 3 -> date==null && country=null  --> highest level of aggregation (only one row with aggregates for the entire data frame)
+
+
+    //
+    // Pivot
+    //
+
+    // ..allows to transform a row into a column
+
+    val pivot:RelationalGroupedDataset = df2.groupBy('date).pivot("Country")
+    pivot.agg(sum('Quantity).as("total_qty"), avg('Quantity).alias("avg_qty")).show()
+
+    // creates one Row per date  with Columns Australia_total_qty | Australia_avg_qty | France_total_qty | France_avg_qty |....
+
+    // Like so:
+    //  +----------+-------------------+-----------------+
+    //  |      date|Australia_total_qty|Australia_avg_qty| ...
+    //  +----------+-------------------+-----------------+
+    //  |2010-12-01|                107|7.642857142857143| ...
+    //  |2010-12-02|               null|             null| ...
+    //  +----------+-------------------+-----------------+
 
 
 
